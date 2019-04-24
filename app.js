ac19269513bac2fabf9b1fdfcba6bdda0873074c
j2c2003 <j2c2003@yahoo.com>
2019-04-24 10:25:43 -0700

    Added Procfile and changed port settings 4 Heroku

--- app.js
+++ app.js
@@ -63,6 +63,8 @@ app.use(function(err, req, res, next) {
 });
 
 // listen on port 3000
-app.listen(3000, function () {
-  console.log('Express app listening on port 3000');
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
